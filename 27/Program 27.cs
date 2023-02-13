2696c90c37c3a4552a937ee92bceb1900b652ca1
Melorini <123016299+Melorini@users.noreply.github.com>
2023-02-13 19:30:36 +0300

    Add files via upload

---
+++ 27/Program 27.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: "); 
+int num = Convert.ToInt32(Console.ReadLine());
+
+  int SumNum(int num)
+  {
+    int count = Convert.ToString(num).Length;
+    int next = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+      next = num - (num % 10);
+      result = result + (num - next);
+      num = num / 10;
+    }
+   return result;
+  }
+
+int Sum = SumNum(num);
+Console.WriteLine("Сумма цифр в числе = " + Sum);
