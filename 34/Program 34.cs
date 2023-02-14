d052b71803645bb65c1b564583175a3f7842b1f3
Melorini <123016299+Melorini@users.noreply.github.com>
2023-02-14 19:33:11 +0300

    Add files via upload

---
+++ 34/Program 34.cs
@@ -0,0 +1,35 @@
+﻿// // Задайте массив заполненный случайными положительными трёхзначными числами.
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+// // [345, 897, 568, 234] -> 2
+
+void FillArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] array) 
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите количество элементов в массиве:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArray(numbers);
+Console.Write("Массив: ");
+PrintArray(numbers);
+
+int count = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+if (numbers[i] % 2 == 0)
+count++;
+}
+Console.WriteLine("Четных чисел: "+ count);
